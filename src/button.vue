39eb18ab2ff06d29d5685d6e4b9d45701969949e
echoheart <limantang01@gmail.com>
2018-08-19 17:00:03 +0800

    npm parcel

---
+++ src/button.vue
@@ -0,0 +1,31 @@
+<template>
+    <g-button></g-button>
+    {{message}}
+</template>
+<script>
+    export default {
+        name: '',
+        template: `
+        <button class="g-button">hi</buttonclass>
+        `
+    }
+</script>
+<style lang="scss">
+
+    .g-button {
+        height: var(--button-height);
+        font-size: var(--font-size);
+        padding: 0 1em;
+        border-radius: var(--button-radius);
+        background: var(--button-bg);
+        &:hover {
+            border-color: var(--button-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+</style>

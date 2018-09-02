de1fbde01f048ec001143f685dfaf77fa62557ef
echoheart <limantang01@gmail.com>
2018-09-02 20:26:38 +0800

    添加测试用例(不包含点击事件)

--- src/app.js
+++ src/app.js
@@ -19,3 +19,68 @@ new Vue({
         loadingThree: true
     }
 });
+
+//  引入chai.js
+
+import chai from 'chai';
+const expect = chai.expect;
+//  单元测试
+{
+    // console.log(Button);
+    const Constructor = Vue.extend(Button);
+    // console.log(Constructor);
+    // console.log(Constructor._init);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    });
+    // console.log(button._init);
+
+    //  不一定要mount到页面内
+    // button.$mount('#test');
+    vm.$mount('#test');
+    // button._init();
+    let useElement = vm.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#icon-setting');
+    vm.$el.remove();
+    vm.$destroy();
+}
+//  测试loading
+{
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting',
+            loading: true
+        }
+    });
+    console.log(vm);
+    vm.$mount();
+    let useElement = vm.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#icon-loading');
+    vm.$el.remove();
+    vm.$destroy();
+}
+//  测试icon左右位置
+{
+    //  必须将元素渲染到页面否则css不能渲染到元素上
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting',
+            iconPosition: 'left'    //  right
+        }
+    });
+    console.log(vm);
+    vm.$mount(div);
+    let svgElement = vm.$el.querySelector('svg');
+    let order = window.getComputedStyle(svgElement).order;
+    expect(order).to.eq('1');   // '2'
+    vm.$el.remove();
+    vm.$destroy();
+}

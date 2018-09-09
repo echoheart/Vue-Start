51f69c9ee4503085dd0a1509e60f6194e76a21e4
echoheart <limantang01@gmail.com>
2018-09-09 19:58:07 +0800

    添加input组件css样式

---
+++ src/input.vue
@@ -0,0 +1,95 @@
+<template>
+    <div
+            class="wrapper"
+            v-bind:class="{'error': error}"
+    >
+        <input
+                v-bind:value="value"
+                v-bind:disabled="disabled"
+                v-bind:readonly="readonly"
+                type="text"
+        >
+        <template v-if="error">
+            <Icon name="error"></Icon>
+            <span class="error-message">{{error}}</span>
+        </template>
+    </div>
+</template>
+
+<script>
+    import Icon from './icon'
+    export default {
+        components: {Icon},
+        name: 'g-input',
+        props: {
+            value: {
+                type: String
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            readonly: {
+                type: Boolean,
+                default: false
+            },
+            error: {
+                type: String
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    /*--button-height: 32px;*/
+    /*--font-size: 14px;*/
+    /*--button-bg: #fff;*/
+    /*--button-active-bg: #eee;*/
+    /*--button-radius: 4px;*/
+    /*--color: #333;*/
+    /*--border-color: #999;*/
+    /*--border-colro-hover: #666;*/
+
+    $border-color: #999;
+    $border-color-hover: #666;
+    $border-radius: 4px;
+    $height: 32px;
+    $font-size: 12px;
+    $box-shadow-color: rgba(0, 0, 0, 0.5);
+    $error-red: #f1453d;
+    .wrapper {
+        font-size: $font-size;
+        display: inline-flex;
+        align-items: center;
+        > :not(:last-child) {
+            margin-right: .5em;
+        }
+        > .error-message {
+            color: red;
+        }
+        &.error {
+            > input {
+                border-color: $error-red;
+            }
+        }
+        > input {
+            height: $height;
+            border: 1px solid $border-color;
+            border-radius: $border-radius;
+            padding: 0 8px;
+            font-size: inherit;
+            &:hover {
+                border-color: $border-color-hover;
+            }
+            &:focus {
+                box-shadow: inset 0 1px 3px $box-shadow-color;
+                outline: none;
+            }
+            &[disabled], &[readonly] {
+                border-color: #333;
+                color: #333;
+                cursor: not-allowed;
+            }
+        }
+    }
+</style>

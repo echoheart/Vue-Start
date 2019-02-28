86bcecbafbb8160587b998ace8ca74b3e49561de
echoheart <limantang01@gmail.com>
2019-02-28 10:36:19 +0800

    添加vuepres 作为官网展示

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'VUE-UI',
+    description: 'UI框架',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/install/' },
+            { text: 'External', link: 'https://google.com' },
+        ],
+        sidebar: [
+            '/',
+            '/install',
+            '/get-start'
+        ]
+    },
+};

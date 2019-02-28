27ccc7385d67419941631d4b91fec1fbf88f9c03
echoHeart <limantang01@gmail.com>
2019-02-28 15:06:03 +0800

    更改导航栏博客地址

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,9 +3,12 @@ module.exports = {
     title: 'VUE-UI',
     description: 'UI框架',
     themeConfig: {
+        head: [
+            ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `./favicon.ico` }]
+        ],
         nav: [
             { text: '首页', link: 'https://echoheart.github.io/Vue-Start/' },
-            { text: '博客', link: 'www.echoheart.cn' },
+            { text: '博客', link: 'https://www.echoheart.cn/' },
             // { text: 'External', link: 'https://google.com' },
         ],
         sidebar: [

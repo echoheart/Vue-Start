bb09746fc5df01b210e0aa568aa861835c9859db
echoHeart <limantang01@gmail.com>
2019-03-02 15:11:55 +0800

    解决了右边视图不更新的bug,由于computed计算属性的原因,导致依赖的rightItem选项没有变化,所以视图没有更新

--- src/cascader-items.vue
+++ src/cascader-items.vue
@@ -56,12 +56,24 @@
         },
         computed: {
             rightItems() {
-                const currentSelected = this.selected[this.level];
-                if (currentSelected && currentSelected.children) {
-                    return this.selected[this.level].children
-                } else {
-                    return null;
+                //  解决右边视图不更新,但是数据更新的问题
+                //  由于computed的计算依赖其他属性,所以如果依赖属性没有变化的话,计算属性也不会变化
+                if (this.selected[this.level]) {
+                    const currentSelected = this.items.filter((item) => {
+                        return item.id === this.selected[this.level].id;
+                    })[0];
+
+                    if(currentSelected && currentSelected.children && currentSelected.children.length > 0) {
+                        return currentSelected.children
+                    }
                 }
+
+                // const currentSelected = this.selected[this.level];
+                // if (currentSelected && currentSelected.children) {
+                //     return this.selected[this.level].children
+                // } else {
+                //     return null;
+                // }
             }
         },
         props: {

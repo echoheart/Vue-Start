16f576fcad0c74388c27a76addc4ffd93dd8e7ed
echoHeart <limantang01@gmail.com>
2019-03-02 20:17:20 +0800

    优化指令,当页面内出现多个相同组件时, 只在document上注册一个事件,利用callbacks数组来执行点击对应组件的事件

--- src/click-outside.js
+++ src/click-outside.js
@@ -1,14 +1,28 @@
 
+let callbacks = [];
 
+document.addEventListener('click', (e) => {
+    callbacks.forEach((item) => {
+        if (item.el === e.target || item.el.contains(e.target)) {
+            return;
+        } else {
+            item.callback();
+        }
+    })
+});
 
 export default {
     bind: function (el, binding) {
-        document.addEventListener('click', (e) => {
-            if (e.target === el || el.contains(e.target)) {
-                return;
-            } else {
-                binding.value();
-            }
+        callbacks.push({
+            el: el,
+            callback: binding.value,
         });
+        // document.addEventListener('click', (e) => {
+        //     if (e.target === el || el.contains(e.target)) {
+        //         return;
+        //     } else {
+        //         binding.value();
+        //     }
+        // });
     }
 }

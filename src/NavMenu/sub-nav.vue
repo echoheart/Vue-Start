0ad2645b42a3a724117ae6b5285b151876906e5b
echoHeart <limantang01@gmail.com>
2019-03-08 14:09:18 +0800

    支持任意层级的nav-item

--- src/NavMenu/sub-nav.vue
+++ src/NavMenu/sub-nav.vue
@@ -32,7 +32,7 @@
         position: relative;
         > span {
             padding: 20px 30px;
-            display: inline-block;
+            display: block;
             vertical-align: top;
         }
         .g-sub-nav-popover {
@@ -42,5 +42,10 @@
             border: 1px solid red;
             white-space: nowrap;
         }
+    }
+    .g-sub-nav .g-sub-nav .g-sub-nav-popover {
+        top: 0;
+        left: 100%;
+        margin-left: 8px;
     }
 </style>

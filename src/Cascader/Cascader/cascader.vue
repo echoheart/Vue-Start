7c5055c84d70245041859155c35606740b1d3021
echoheart <limantang01@gmail.com>
2019-10-31 20:33:19 +0800

    格式化代码

--- src/Cascader/Cascader/cascader.vue
+++ src/Cascader/Cascader/cascader.vue
@@ -1,177 +1,178 @@
 <template>
-    <div class="cascader" ref="cascader" v-click-out-side="close">
-        <div class="trigger" v-on:click="toggle">
-            {{ result || '&nbsp'}}
-        </div>
-        <div class="popover-wrapper" v-if="popoverVisible">
-            <CascaderItems
-                    v-bind:items="source"
-                    class="popover"
-                    v-bind:height="popoverHeight"
-                    v-bind:selected="selected"
-                    v-on:update:selected="onUpdate"
-                    v-bind:load-data="loadData"
-                    v-bind:shouldClose="close"
-                    v-bind:loadingItem="loadingItem"
-            >
-            </CascaderItems>
-        </div>
+  <div class="cascader" ref="cascader" v-click-out-side="close">
+    <div class="trigger" v-on:click="toggle">
+      {{ result || '&nbsp'}}
     </div>
+    <div class="popover-wrapper" v-if="popoverVisible">
+      <CascaderItems
+        v-bind:items="source"
+        class="popover"
+        v-bind:height="popoverHeight"
+        v-bind:selected="selected"
+        v-on:update:selected="onUpdate"
+        v-bind:load-data="loadData"
+        v-bind:shouldClose="close"
+        v-bind:loadingItem="loadingItem"
+      >
+      </CascaderItems>
+    </div>1
+  </div>
 </template>
 
 <script>
-    import CascaderItems from '../CascaderItem/cascader-items';
-    import clickOutSide from '../../utils/click-outside';
-    export default {
-        name: 'Cascader',
-        components: {
-            CascaderItems
-        },
-        directives: {clickOutSide},
-        methods: {
-            // onHandleDocumentClick(e) {
-            //     const { cascader } = this.$refs;
-            //     if (e.target === cascader || cascader.contains(e.target)) {
-            //         return;
-            //     }
-            //     this.close();
-            // },
-            toggle() {
-              if (this.popoverVisible) {
-                  this.close();
-              } else {
-                  this.open();
-              }
-            },
-            open() {
-              this.popoverVisible = true;
-              // document.addEventListener('click', this.onHandleDocumentClick);
-            },
-            close() {
-              this.popoverVisible = false;
-              // document.removeEventListener('click', this.onHandleDocumentClick)
-            },
-            onUpdate(copySelected) {
-                //  查找
-                let simplest = (children, id) => {
-                    const found = children.filter((item) => {
-                        return item.id === id;
-                    });
-                    return found[0];
-                };
+  import CascaderItems from '../CascaderItem/cascader-items';
+  import clickOutSide from '../../utils/click-outside';
 
-                let complex = (children, id) => {
-                    let noChildren = [];
-                    let hasChildren = [];
-                    children.forEach((item) => {
-                        if (item.children) {
-                            hasChildren.push(item)
-                        } else {
-                            noChildren.push(item)
-                        }
-                    });
-                    let found = simplest(noChildren, id);
-                    if (found) {
-                        return found;
-                    } else {
-                        found = simplest(hasChildren, id);
-                        if (found) {
-                            return found
-                        } else {
-
-                            for (let i = 0; i < hasChildren.length; i++) {
-                                found = complex(hasChildren[i].children, id);
-                                if(found) {
-                                    return found
-                                }
-                            }
-                        }
-                        return undefined;
-                    }
-                };
-
-                this.$emit('update:selected', copySelected);
-                const lastSelectedItem = copySelected[copySelected.length - 1];
-                const {id} = lastSelectedItem;
-                console.log(this.source);
-                console.log(id);
-                let updateSource = (result) => {
-                    const copySource = JSON.parse(JSON.stringify(this.source));
-                    const selectedItem = complex(copySource, id);
-                    selectedItem.children = result;
-                    // console.log(selectedItem);
-                    // this.$set(selectedItem, 'children', result);
-                    this.$emit('update:source', copySource);
-                    this.loadingItem = {};
-
-                };
-                if (!lastSelectedItem.isLeaf && this.loadData) {
-                    this.loadData(id, updateSource);
-                    this.loadingItem = lastSelectedItem;
-                }
-            }
-        },
-        computed: {
-            result() {
-                return this.selected.map((item) => {
-                    return item.name;
-                }).join('/');
-            }
-        },
-        data() {
-            return {
-                popoverVisible: false,
-                loadingItem: {}
-            }
-        },
-        props: {
-            source: {
-                type: Array
-            },
-            popoverClassName: {
-                type: String,
-            },
-            popoverHeight: {
-                type: String,
-            },
-            selected: {
-                type: Array,
-                default: () => []
-            },
-            loadData: {
-                type: Function,
-            }
-
+  export default {
+    name: 'Cascader',
+    components: {
+      CascaderItems
+    },
+    directives: {clickOutSide},
+    methods: {
+      // onHandleDocumentClick(e) {
+      //     const { cascader } = this.$refs;
+      //     if (e.target === cascader || cascader.contains(e.target)) {
+      //         return;
+      //     }
+      //     this.close();
+      // },
+      toggle() {
+        if (this.popoverVisible) {
+          this.close();
+        } else {
+          this.open();
         }
+      },
+      open() {
+        this.popoverVisible = true;
+        // document.addEventListener('click', this.onHandleDocumentClick);
+      },
+      close() {
+        this.popoverVisible = false;
+        // document.removeEventListener('click', this.onHandleDocumentClick)
+      },
+      onUpdate(copySelected) {
+        //  查找
+        let simplest = (children, id) => {
+          const found = children.filter((item) => {
+            return item.id === id;
+          });
+          return found[0];
+        };
+
+        let complex = (children, id) => {
+          let noChildren = [];
+          let hasChildren = [];
+          children.forEach((item) => {
+            if (item.children) {
+              hasChildren.push(item)
+            } else {
+              noChildren.push(item)
+            }
+          });
+          let found = simplest(noChildren, id);
+          if (found) {
+            return found;
+          } else {
+            found = simplest(hasChildren, id);
+            if (found) {
+              return found
+            } else {
+
+              for (let i = 0; i < hasChildren.length; i++) {
+                found = complex(hasChildren[i].children, id);
+                if (found) {
+                  return found
+                }
+              }
+            }
+            return undefined;
+          }
+        };
+
+        this.$emit('update:selected', copySelected);
+        const lastSelectedItem = copySelected[copySelected.length - 1];
+        const {id} = lastSelectedItem;
+        console.log(this.source);
+        console.log(id);
+        let updateSource = (result) => {
+          const copySource = JSON.parse(JSON.stringify(this.source));
+          const selectedItem = complex(copySource, id);
+          selectedItem.children = result;
+          // console.log(selectedItem);
+          // this.$set(selectedItem, 'children', result);
+          this.$emit('update:source', copySource);
+          this.loadingItem = {};
+
+        };
+        if (!lastSelectedItem.isLeaf && this.loadData) {
+          this.loadData(id, updateSource);
+          this.loadingItem = lastSelectedItem;
+        }
+      }
+    },
+    computed: {
+      result() {
+        return this.selected.map((item) => {
+          return item.name;
+        }).join('/');
+      }
+    },
+    data() {
+      return {
+        popoverVisible: false,
+        loadingItem: {}
+      }
+    },
+    props: {
+      source: {
+        type: Array
+      },
+      popoverClassName: {
+        type: String,
+      },
+      popoverHeight: {
+        type: String,
+      },
+      selected: {
+        type: Array,
+        default: () => []
+      },
+      loadData: {
+        type: Function,
+      }
+
     }
+  }
 </script>
 
 <style scoped lang="less">
-    @import "../../var.less";
+  @import "../../var.less";
 
-    .cascader {
-        position: relative;
+  .cascader {
+    position: relative;
 
-        .trigger {
-            border: 1px solid @border-color;
-            height: @input-height;
-            display: inline-flex;
-            align-items: center;
-            padding: 0 1em;
-            min-width: 10em;
-            border-radius: @border-radius;
-        }
-
-        .popover-wrapper {
-            @extend .box-shadow;
-            margin-top: 5px;
-            position: absolute;
-            top: 100%;
-            left: 0;
-            background-color: white;
-            display: flex;
-            justify-content: flex-start;
-            height: 200px;
-        }
+    .trigger {
+      border: 1px solid @border-color;
+      height: @input-height;
+      display: inline-flex;
+      align-items: center;
+      padding: 0 1em;
+      min-width: 10em;
+      border-radius: @border-radius;
     }
+
+    .popover-wrapper {
+    @extend . box-shadow;
+      margin-top: 5px;
+      position: absolute;
+      top: 100%;
+      left: 0;
+      background-color: white;
+      display: flex;
+      justify-content: flex-start;
+      height: 200px;
+    }
+  }
 </style>
--- src/Cascader/CascaderItem/cascader-items.vue
+++ src/Cascader/CascaderItem/cascader-items.vue
@@ -1,168 +1,172 @@
 <template>
-    <div class="source-item" v-bind:style="{height: height}">
-        <!--selected: {{ selected[level] && selected[level].name }}-->
-        <!--level: {{ level }}-->
-        <!--{{ rightItems }}-->
-        <div class="left">
-            <div class="label"
-                 v-for="item in items"
-                 v-on:click="onClickHandle(item)">
+  <div class="source-item" v-bind:style="{height: height}">
+    <!--selected: {{ selected[level] && selected[level].name }}-->
+    <!--level: {{ level }}-->
+    <!--{{ rightItems }}-->
+    <div class="left">
+      <div class="label"
+           v-for="item in items"
+           v-on:click="onClickHandle(item)">
 
-                <span class="name">{{ item.name }}</span>
-                <span class="icons">
-                    <template v-if="loadingItem.name === item.name">
-                        <Icon class="icon loading" name="loading"></Icon>
-                    </template>
-                    <template v-else>
-                        <Icon class="icon" name="right" v-if="isShowRightArrow(item)"></Icon>
-                    </template>
-                </span>
-            </div>
-        </div>
-        <div class="right" v-if="rightItems">
-            <CascaderItems
-                    v-bind:items="rightItems"
-                    v-bind:height="height"
-                    v-bind:selected="selected"
-                    v-bind:level="level + 1"
-                    v-on:update:selected="onUpdate"
-                    v-bind:load-data="loadData"
-                    v-bind:should-close="shouldClose"
-                    v-bind:loadingItem="loadingItem"
-            >
-            </CascaderItems>
-        </div>
-
+        <span class="name">{{ item.name }}</span>
+        <span class="icons">
+            <template v-if="loadingItem.name === item.name">
+                <Icon class="icon loading" name="loading"></Icon>
+            </template>
+            <template v-else>
+                <Icon class="icon" name="right" v-if="isShowRightArrow(item)"></Icon>
+            </template>
+        </span>
+      </div>
     </div>
+    <div class="right" v-if="rightItems">
+      <CascaderItems
+        v-bind:items="rightItems"
+        v-bind:height="height"
+        v-bind:selected="selected"
+        v-bind:level="level + 1"
+        v-on:update:selected="onUpdate"
+        v-bind:load-data="loadData"
+        v-bind:should-close="shouldClose"
+        v-bind:loadingItem="loadingItem"
+      >
+      </CascaderItems>
+    </div>
+
+  </div>
 </template>
 
 <script>
-    import Icon from '../../Icon/icon';
+  import Icon from '../../Icon/icon';
 
-    const component = {
-        name: 'CascaderItems',
-        components: {
-            CascaderItems: 'CascaderItems',
-            Icon: Icon,
-        },
-        methods: {
-            isShowRightArrow(item) {
-                return this.loadData ? !item.isLeaf : item.children;
-            },
-            onClickHandle(item) {
-                const copySelected = this.selected.slice();
-                //    更新时删除当前层级的后的所有元素
-                copySelected.splice(this.level);
+  const component = {
+    name: 'CascaderItems',
+    components: {
+      CascaderItems: 'CascaderItems',
+      Icon: Icon,
+    },
+    methods: {
+      isShowRightArrow(item) {
+        return this.loadData ? !item.isLeaf : item.children;
+      },
+      onClickHandle(item) {
+        const copySelected = this.selected.slice();
+        //    更新时删除当前层级的后的所有元素
+        copySelected.splice(this.level);
 
-                copySelected[this.level] = item;
+        copySelected[this.level] = item;
 
-                this.$emit('update:selected', copySelected);
+        this.$emit('update:selected', copySelected);
 
-                if (!this.isShowRightArrow(item)) {
-                    this.shouldClose()
-                }
-            },
-            onUpdate(copySelected) {
-                this.$emit('update:selected', copySelected)
-            }
-        },
-        data() {
-            return {
-
-            }
-        },
-        computed: {
-            rightItems() {
-                //  解决右边视图不更新,但是数据更新的问题
-                //  由于computed的计算依赖其他属性,所以如果依赖属性没有变化的话,计算属性也不会变化
-                if (this.selected[this.level]) {
-                    const currentSelected = this.items.filter((item) => {
-                        return item.name === this.selected[this.level].name;
-                    })[0];
-
-                    if(currentSelected && currentSelected.children && currentSelected.children.length > 0) {
-                        return currentSelected.children
-                    }
-                }
-
-                // const currentSelected = this.selected[this.level];
-                // if (currentSelected && currentSelected.children) {
-                //     return this.selected[this.level].children
-                // } else {
-                //     return null;
-                // }
-            }
-        },
-        props: {
-            items: {
-                type: Array
-            },
-            height: {
-                type: String
-            },
-            selected: {
-                type: Array,
-                default: () => []
-            },
-            level: {
-                type: Number,
-                default: 0
-            },
-            loadData: {
-                type: Function,
-            },
-            shouldClose: {
-                type: Function
-            },
-            loadingItem: {
-                type: Object,
-                default: () => {
-                    return {};
-                }
-            }
+        if (!this.isShowRightArrow(item)) {
+          this.shouldClose()
         }
-    };
-    export default component
+      },
+      onUpdate(copySelected) {
+        this.$emit('update:selected', copySelected)
+      }
+    },
+    data() {
+      return {}
+    },
+    computed: {
+      rightItems() {
+        //  解决右边视图不更新,但是数据更新的问题
+        //  由于computed的计算依赖其他属性,所以如果依赖属性没有变化的话,计算属性也不会变化
+        if (this.selected[this.level]) {
+          const currentSelected = this.items.filter((item) => {
+            return item.name === this.selected[this.level].name;
+          })[0];
+
+          if (currentSelected && currentSelected.children && currentSelected.children.length > 0) {
+            return currentSelected.children
+          }
+        }
+
+        // const currentSelected = this.selected[this.level];
+        // if (currentSelected && currentSelected.children) {
+        //     return this.selected[this.level].children
+        // } else {
+        //     return null;
+        // }
+      }
+    },
+    props: {
+      items: {
+        type: Array
+      },
+      height: {
+        type: String
+      },
+      selected: {
+        type: Array,
+        default: () => []
+      },
+      level: {
+        type: Number,
+        default: 0
+      },
+      loadData: {
+        type: Function,
+      },
+      shouldClose: {
+        type: Function
+      },
+      loadingItem: {
+        type: Object,
+        default: () => {
+          return {};
+        }
+      }
+    }
+  };
+  export default component
 
 </script>
 
 <style lang="less" scoped>
-    @import '../../var.less';
+  @import '../../var.less';
 
-    .source-item {
+  .source-item {
 
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+
+    .left {
+      padding-top: .3em;
+      overflow: auto;
+      height: 100%;
+
+      .label {
         display: flex;
-        align-items: flex-start;
-        justify-content: flex-start;
+        cursor: pointer;
+        user-select: none;
 
-        .left {
-            padding-top: .3em;
-            overflow: auto;
-            height: 100%;
-            .label {
-                display: flex;
-                cursor: pointer;
-                user-select: none;
-                &:hover {
-                    background: @grey;
-                }
-                padding: .5em 1em;
-                > .name {
-                    padding-right: 1em;
-                }
-                > .icons {
-                    margin-left: auto;
-                    >  .loading {
-                        animation: spin 1s infinite linear;
-                    }
-                }
-            }
+        &:hover {
+          background: @grey;
         }
 
-        .right {
-            height: 100%;
-            border-left: 1px solid @grey;
+        padding: .5em 1em;
+
+        > .name {
+          padding-right: 1em;
         }
 
+        > .icons {
+          margin-left: auto;
+
+          > .loading {
+            animation: spin 1s infinite linear;
+          }
+        }
+      }
     }
+
+    .right {
+      height: 100%;
+      border-left: 1px solid @grey;
+    }
+
+  }
 </style>

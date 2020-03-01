4c589dfd72cf165f7d278169e653b32653f75306
echoheart <limantang01@gmail.com>
2020-03-01 22:07:39 +0800

    支持滚动条

--- src/Scroll/Scroll.vue
+++ src/Scroll/Scroll.vue
@@ -3,14 +3,26 @@
     <div class="scroll" ref="child">
       <slot></slot>
     </div>
+
+    <div class="scroll-track" >
+      <div class="scroll-bar" ref="bar">
+        <div class="scroll-bar-inner"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
     name: "Scroll",
+    data() {
+      return {
+        innerBarHeight: 0
+      }
+    },
     mounted() {
       console.log('scroll mounted');
+
       const parent = this.$refs.parent;
       const child = this.$refs.child;
       let translateY = 0;
@@ -42,20 +54,56 @@
         }
 
 
-        child.style.transform = `translateY(${translateY}px)`
+        child.style.transform = `translateY(${translateY}px)`;
+        this.updateScrollHeight(parentHeight, childHeight, translateY);
+      });
 
-      })
+    },
+    methods: {
+      updateScrollHeight(parentHeight, childHeight, translateY) {
+        const barHeight = parentHeight * parentHeight / childHeight;
+        this.$refs.bar.style.height = barHeight + 'px';
+
+        const y = parentHeight * translateY / childHeight;
+        this.$refs.bar.style.transform = `translateY(${-y}px)`
+      }
     }
   }
 </script>
 
 <style scoped lang="less">
   .scroll-wrapper {
-    border: 5px solid red;
+    border: 1px solid grey;
     overflow: hidden;
+    position: relative;
     .scroll {
-      border: 2px solid green;
       transition: transform 0.02s ease;
+    }
+    .scroll-track {
+      position: absolute;
+      top: 0;
+      right: 0;
+      height: 100%;
+      width: 14px;
+      background-color: #FAFAFA;
+      border-left: 1px solid #E8E7E8;
+    }
+    .scroll-bar {
+      position: absolute;
+      top: -1px;
+      left: 50%;
+      height: 40px;
+      width: 8px;
+      margin-left: -4px;
+      transition: transform 0.02s ease;
+      &-inner {
+        background: #C2C2C2;
+        border-radius: 4px;
+        height: 100%;
+        &:hover {
+          background: #7d7d7d;
+        }
+      }
     }
   }
 </style>
